a7bc9dced815c6636900290d95141081d95e1613
rick <github email address>
2020-03-30 09:56:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  var b = document.getElementById('main-content')
+  let submit_button = document.querySelector('input[type="submit"]');
+  var ordered_list = document.createElement("ol");
+  let form = document.getElementById('create-task-form')
+  b.appendChild(ordered_list)
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+  })
+  submit_button.addEventListener('click', function e () {
+
+    let new_li = document.createElement("li")
+    let new_item = document.getElementById('new-task-description').value;
+    let node = document.createTextNode(new_item)
+    new_li.appendChild(node)
+    document.querySelector('ol').appendChild(new_li);
+  })
 });
